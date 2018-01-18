78066f8cdd64284eef93186f32b0bc650ed2229b
gkaran <gkaran555@gmail.com>
2018-01-18 19:01:31 +0100

    1

---
+++ resources/assets/js/components/Myheader.vue
@@ -0,0 +1,13 @@
+<template>
+	<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+
+    <div class="navbar-menu is-active">
+      <router-link to="/home" class="navbar-item">Home</router-link>
+      <router-link to="/about" class="navbar-item">About</router-link>
+    </div>
+
+   
+  </div>
+</nav>
+</template>
